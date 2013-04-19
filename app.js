c57bdbbb4b3c24868db81fb24745a6efeb98fbfa
Anant Narayanan <anant@kix.in>
2013-04-19 12:26:02 -0700

    Sanitize disallowed Firebase keys

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ ref.auth(process.env.SECRET, function(err) {
     console.log("Firebase authentication failed!", err);
   } else {
     setupHandlers();
-    setInterval(parseFeeds, 1200000);
+    setInterval(parseFeeds, 600000);
   }
 });
 
@@ -38,46 +38,81 @@ function editUserFeed(snap) {
   parseFeeds();
 }
 
+function sanitizeObject(obj) {
+  if (typeof obj != typeof {}) {
+    return obj;
+  }
+
+  var newObj = {};
+  var special = [".", "$", "/", "[", "]"];
+  for (var key in obj) {
+    var sum = -1;
+    for (var i in special) {
+      sum += (key.indexOf(special[i])) + 1;
+    }
+    if (sum < 0) {
+      if (key == "date" || key == "pubdate" || key == "pubDate") {
+        if (obj[key]) {
+          newObj[key] = obj[key].toString();
+        }
+      } else if (key == "#") {
+        newObj["value"] = sanitizeObject(obj[key]);
+      } else if (key.indexOf("#") >= 0) {
+        newObj["@" + key.replace("#", "")] = sanitizeObject(obj[key]);
+      } else if (sanitizeObject(obj[key]) && key != "") {
+        newObj[key] = sanitizeObject(obj[key]);
+      }
+    }
+  }
+  return newObj;
+}
+
 function parseFeeds() {
   for (var id in feeds) {
-    var abort = false;
-    var feed = feeds[id];
-    request(feed.value.url, function(err) {
-      if (err) {
-        abort = true;
-        feed.status.set(err);
-      }  
-    })
-      .pipe(new Parser())
-      .on("error", function(err) {
-        abort = true;
-        feed.status.set(err);
-      })
-      .on("meta", function(meta) {
-        try {
-          var fbRef = new Firebase(feed.value.firebase);
-          fbRef.child("meta").set(meta);
-        } catch(e) {
-          abort = true;
-          feed.status.set(e.toString());
-        }
-      })
-      .on("article", function(article) {
-        var id = article.guid || article.link || article.title;
-        id = new Buffer(id).toString("base64");
-        try {
-          var fbRef = new Firebase(feed.value.firebase);
-          fbRef.child(id).set(article);
-        } catch(e) {
-          abort = true;
-          console.log(e);
-          feed.status.set(e.toString());
-        }
-      })
-      .on("end", function() {
-        if (!abort) {
-          feed.status.set("Last Sync: " + new Date());
-        }
-      });
+    getAndSetFeed(feeds[id]);
   }
+}
+
+function getAndSetFeed(feed) {
+  var abort = false;
+  request(feed.value.url, function(err) {
+    if (err) {
+      abort = true;
+      feed.status.set(err);
+    }  
+  }).
+  pipe(new Parser({addmeta: false})).
+  on("error", function(err) {
+    abort = true;
+    feed.status.set(err);
+  }).
+  on("meta", function(meta) {
+    try {
+      var fbRef = new Firebase(feed.value.firebase);
+      fbRef.child("meta").set(sanitizeObject(meta));
+    } catch(e) {
+      abort = true;
+      console.log("Error for object: ");
+      console.log(sanitizeObject(meta));
+      feed.status.set(e.toString());
+    }
+  }).
+  on("article", function(article) {
+    var id = article.guid || article.link || article.title;
+    id = new Buffer(id).toString("base64");
+    try {
+      var fbRef = new Firebase(feed.value.firebase);
+      fbRef.child("articles/" + id).set(sanitizeObject(article));
+    } catch(e) {
+      abort = true;
+      console.log("Error for object: ");
+      console.log(sanitizeObject(article));
+      feed.status.set(e.toString());
+    }
+  }).
+  on("end", function() {
+    if (!abort) {
+      feed.status.set("Last Sync:<br/>" + new Date());
+    }
+  });
 }
